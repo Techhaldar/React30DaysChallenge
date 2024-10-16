fa28084d98ca48a948838a7e2be60d7e50e6e1f0
Ratin <ratinhaldar123@gmail.com>
2024-10-16 15:44:55 +0530

    JavaScript Expression in JSX

---
+++ Practice_Series/JavaScript_Expression/Expression/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NameInjecting from './Components/NameInjecting'
+function App() {
+  
+
+  return (
+    <>
+      <NameInjecting />
+    </>
+  )
+}
+
+export default App
---
+++ Practice_Series/JavaScript_Expression/Expression/src/Components/NameInjecting.jsx
@@ -0,0 +1,7 @@
+function NameInjecting () {
+  const name = ("Ratin Haldar");
+  
+  return <h4>Hello, {name} this side</h4>
+}
+
+export default NameInjecting
