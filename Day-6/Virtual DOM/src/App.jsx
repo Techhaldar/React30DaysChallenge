f94c98783ed6772f38bdf421374189c3673f8545
Ratin <ratinhaldar123@gmail.com>
2024-11-17 23:05:05 +0530

    Virtual Dom & revision last day code

--- Day-6/Virtual DOM/src/App.jsx
+++ Day-6/Virtual DOM/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter, setcounter] = useState(15)
+
+  const addValue = () => {
+    if (counter < 24) {
+      setcounter(counter + 1)
+    } else {
+      setcounter(counter)
+
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setcounter(counter - 1)
+    } else {
+      setcounter(counter)
+    }
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Add Value & Remove Value</h1>
+      <h1>Counter Value : {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
