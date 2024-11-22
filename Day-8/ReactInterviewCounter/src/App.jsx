982f68752a2857d48ea23d3297963a2423b970e1
Ratin <ratinhaldar123@gmail.com>
2024-11-22 15:15:06 +0530

    React Interview question on counter

--- Day-8/ReactInterviewCounter/src/App.jsx
+++ Day-8/ReactInterviewCounter/src/App.jsx
@@ -3,29 +3,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setcount] = useState(10)
 
-  function addValue() {
-    setCount(count => count + 1)
-    setCount(count => count + 1)
-    setCount(count => count + 1)
-    setCount(count => count + 1)
+  const addValue = () => {
+    setcount(count + 1);
+    setcount(count => count + 1);
+    setcount(count => count + 1);
 
   }
-  function removeValue() {
-    if (count > 0) {
-      setCount(count - 1)
-    }
+  const removeValue = () => {
+    setcount(count - 1);
   }
+
 
   return (
     <>
-
-      <h1 className="text-3xl font-bold underline">Counter Value: {count}</h1>
-      <button className='btn' onClick={addValue}>Add Button {count}</button>
-      <button className='btn' onClick={removeValue}>Remove Button {count}</button>
-
-
+      <div>Value: {count}</div>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
