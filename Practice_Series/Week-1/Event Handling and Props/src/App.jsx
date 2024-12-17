900c221a625e916ac743341469fc0dd7dcb62f3a
Ratin <ratinhaldar123@gmail.com>
2024-12-17 16:22:48 +0530

    Event Handling and Props

---
+++ Practice_Series/Week-1/Event Handling and Props/src/App.jsx
@@ -0,0 +1,9 @@
+import Button from "./components/Button";
+
+
+const App = () => {
+return (
+    <Button myName="Ratin Haldar" email="ratinhaldar123@gmail.com" age="22" Mobile="7351721372"></Button>
+  )
+};
+export default App;
---
+++ Practice_Series/Week-1/Event Handling and Props/src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ myName, email, age , Mobile }) => {
+  return <>
+
+    <h1>Name: {myName}</h1>
+    <h2>Email ID: {email}</h2>
+    <h3>Age: {age}</h3>
+    <h3>Mobile:{Mobile}</h3>
+  </>
+
+}
+
+export default Button;
